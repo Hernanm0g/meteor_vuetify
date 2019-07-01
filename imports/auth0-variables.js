f103fca57a0d08b9a7b3796de410849776d64ae4
Hernán Mogollón <hernanmog@gmail.com>
2019-06-30 22:55:12 -0500

    auth0 vars

--- imports/auth0-variables.js
+++ imports/auth0-variables.js
@@ -1,7 +1,7 @@
 
 const AUTH0 = {
-    CLIENT_ID: 'EcVtt1PoK8fUDCf6paFeh3H6eFfQOtBa', // Change it to your app client id
-    DOMAIN:'totoom.eu.auth0.com', // Change it to your auth0 domain
+    CLIENT_ID: '', // Change it to your app client id
+    DOMAIN:'', // Change it to your auth0 domain
     CALLBACK:'http://localhost:3000' // Change it to your production url
 };
 
