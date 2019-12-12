8050a059460493c0453cdf7ffa8d4fd7aa58b30f
Hernán Mogollón <hernanmog@gmail.com>
2019-12-12 14:38:34 -0500

    removed node_modules

--- node_modules/vuetify/lib/services/index.js
+++
@@ -1,7 +0,0 @@
-export * from './application';
-export * from './breakpoint';
-export * from './goto';
-export * from './icons';
-export * from './lang';
-export * from './theme';
-//# sourceMappingURL=index.js.map
