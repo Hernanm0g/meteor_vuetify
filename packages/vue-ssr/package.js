14b3f59a19942a571563dc89e34d5dc11fb9cd94
Hernanm0g <hernanmog@gmail.com>
2021-05-09 15:34:20 -0500

    #19 Added vue-ssr to packages

---
+++ packages/vue-ssr/package.js
@@ -0,0 +1,33 @@
+Package.describe({
+  name: 'akryum:vue-ssr',
+  version: '0.4.0',
+  summary: 'Render Vue server-side',
+  git: 'https://github.com/Akryum/meteor-vue-component',
+  documentation: 'README.md',
+})
+
+Package.onUse(function (api) {
+  api.versionsFrom('1.6.1')
+  api.use([
+    'isobuild:compiler-plugin@1.0.0',
+    'ecmascript',
+    'tracker',
+    'minimongo',
+    'underscore',
+    'webapp',
+    'mongo',
+    'routepolicy',
+    'url',
+    'akryum:npm-check@0.1.1',
+    'staringatlights:fast-render@3.2.0',
+    'ejson',
+    'server-render',
+  ])
+  api.mainModule('server/index.js', 'server')
+  api.export('VueSSR', 'server')
+})
+
+Npm.depends({
+  'vue-server-renderer': '2.6.12',
+  'cookie-parser': '1.4.4',
+})
